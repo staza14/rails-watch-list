1c4a93b4a65506db26212f842dabc8f7aa498af9
Anastasia Florentis <anastasiaflorentis@gmail.com>
2023-08-18 19:39:16 +0100

    working on front end

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ gem "bootsnap", require: false
 
 # Use Sass to process CSS
 gem "sassc-rails"
+gem "cloudinary"
 
 gem "bootstrap", "~> 5.2"
 gem "autoprefixer-rails"
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,5 @@
 class ListsController < ApplicationController
+
   def index
     @lists = List.all
   end
@@ -21,6 +22,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
